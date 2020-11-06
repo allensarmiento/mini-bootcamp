3940aacdf34ed6e93d7a7fea383c5c707d15e7d0
allensarmiento <allensarmiento3@gmail.com>
2020-11-06 15:08:47 -0800

    Implement slides and button to move forward

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
     <router-view />
-    <b-alert show>Default Alert</b-alert>
   </div>
 </template>
 
 <script>
-import { BAlert } from 'bootstrap-vue';
-
 export default {
   name: 'App',
-  components: {
-    BAlert
-  }
+  components: {}
 }
 </script>
 
-<style>
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
+}
+
+html {
+  font-size: 62.5%;
+
+  --dark-primary-color: #455A64;
+  --light-primary-color: #CFD8DC;
+  --primary-color: #607D8B;
+  --text: #FFF;
+  --icons: #FFF;
+  --accent-color: #448AFF;
+  --primary-text: #212121;
+  --secondary-text: #757575;
+  --divider-color: #BDBDBD;
+}
+
+body {
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  // margin-top: 60px;
 }
 </style>
--- client/src/components/Lesson.vue
+++ client/src/components/Lesson.vue
@@ -1,14 +1,91 @@
 <template>
-  <div>
-    <h1>Lesson</h1>
-    <p>{{ slides[slideIndex].title }}</p>
-    <p>{{ items[itemIndex] }}</p>
-    <button>Move Back</button>
-    <button @click="moveForward">Move Forward</button>
+  <div class="lesson">
+    <h1 class="lesson__title">Lesson 1</h1>
+
+    <b-jumbotron class="lesson-slide" container-fluid>
+      <h2 class="lesson-slide__title">
+        {{ slides[slideIndex].title }}
+      </h2>
+
+      <div class="lesson-slide__content">
+        <div v-for="(displayItem, key) in displayItems" :key="key">
+          <p v-if="displayItem.type === 'text'">{{ displayItem.text }}</p>
+          <div 
+            v-if="displayItem.type === 'image'"
+            class="lesson-slide__content__image-container"
+          >
+            <img 
+              v-if="displayItem.type === 'image'"
+              class="lesson-slide__content__image"
+              :src="displayItem.image"
+            /> 
+          </div>
+        </div>
+      </div>
+
+      <div class="lesson-slide__controls">
+        <b-button 
+          class="lesson-slide__controls__item" 
+          variant="primary" 
+          href="#"
+        >
+          &larr; Move Back
+        </b-button>
+
+        <b-button 
+          v-b-toggle.sidebar-right
+          class="lesson-slide__controls__item"
+        >
+          Toggle Sidebar
+        </b-button>
+
+        <b-button 
+          class="lesson-slide__controls__item" 
+          variant="primary" 
+          @click="moveForward"
+        >
+          Move Forward &rarr;
+        </b-button>
+      </div>
+    </b-jumbotron>
+
+    <b-sidebar 
+      id="sidebar-right"
+      class="lesson-sidebar" 
+      title="Name's Answers" 
+      right 
+      shadow
+    >
+      <div class="lesson-sidebar__container px-3 py-4">
+        <div class="lesson-sidebar__content">
+          <div v-for="(displayItem, key) in displayItems" :key="key">
+            <p v-if="displayItem.type === 'text'">{{ displayItem.text }}</p>
+          </div>
+        </div>
+
+        <div class="lesson-sidebar__input__container">
+          <b-form-textarea
+            id="textarea"
+            class="lesson-sidebar__input"
+            v-model="text"
+            placeholder="Enter something..."
+            rows="5"
+            max-rows="6"
+          ></b-form-textarea>
+          <b-button 
+            class="lesson-sidebar__submit"
+          >
+            Submit
+          </b-button>
+        </div>
+      </div>
+    </b-sidebar>
   </div>
 </template>
 
 <script>
+import lesson from '../mock/lesson_1_slides.json'
+
 export default {
   name: 'Lesson',
   data: function() {
@@ -16,39 +93,33 @@ export default {
       slideIndex: 0,
       itemIndex: 0,
 
-      slides: [
-        {
-          title: 'Slide 1',
-          items: [
-            'Test 1:1',
-            'Test 1:2'
-          ]
-        },
-        {
-          title: 'Slide 2',
-          items: [
-            'Test 2:1',
-            'Test 2:2'
-          ]
-        }
-      ],
-      items: []
+      slides: lesson,
+
+      displayItems: []
     }
   },
   mounted: function() {
-    this.items = this.slides[0].items
+
   },
   methods: {
     moveForward: function() {
-      if (this.itemIndex < this.items.length - 1) {
+      if (this.itemIndex < this.slides[this.slideIndex].items.length) {
+        this.displayItems
+          .push(this.slides[this.slideIndex].items[this.itemIndex])
         this.itemIndex += 1
-      } else if (this.slideIndex < this.slides.length - 1) {
-        this.slideIndex += 1
-        this.items = this.slides[this.slideIndex].items
-        this.itemIndex = 0
+      } else {
+        if (this.slideIndex < this.slides.length) {
+          this.slideIndex += 1
+          this.itemIndex = 0
+          this.displayItems = []
+        }
       }
 
-      if (this.itemIndex === this.items.length - 1 && this.slideIndex === this.slides.length - 1) {
+      if (
+        this.itemIndex === this.slides[this.slideIndex].items.length && 
+        this.slideIndex === this.slides.length
+      ) {
+        this.displayItems = ['The end']
         console.log('The end!')
       }
     }
@@ -56,6 +127,127 @@ export default {
 }
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+  .lesson {
+    display: grid;
+    grid-template-rows: [title-start] 10vh [title-end
+                        main-start] 90vh [main-end];
 
+    &__title {
+      grid-row: title-start / title-end;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      height: 10vh;
+      background-color: var(--primary-color);
+      color: var(--text);
+      font-size: 5rem;
+    }
+  }
+
+  .lesson-slide {
+    grid-row: main-start / main-end;
+
+    display: grid;
+    grid-template-rows: [title-start] min-content [title-end
+                        main-start] 1fr [main-end
+                        controls-start] min-content [controls-end];
+
+    padding: 0;
+    height: 90vh;
+    background-color: var(--light-primary-color);
+    color: var(--primary-text);
+
+    &__title {
+      grid-row: title-start / title-end;
+      padding: 1rem 2rem;
+      font-size: 3.6rem;
+      text-align: left;
+      color: currentColor;
+    }
+
+    &__content {
+      grid-row: main-start / main-end;
+      padding: 1rem 4rem;
+      font-size: 2.6rem;
+      text-align: left;
+      color: currentColor;
+      overflow: auto;
+
+      &__image-container {
+        width: 50rem;
+        height: 30rem;
+        border-radius: .4rem;
+        overflow: hidden;
+      }
+
+      &__image {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
+
+    &__controls {
+      grid-row: controls-start / controls-end;
+
+      display: flex;
+      justify-content: space-between;
+
+      padding: 2rem 4rem;
+
+      background: var(--primary-color);
+
+      &__item {
+        padding: .8rem 1.2rem;
+        border: 1px solid var(--dark-primary-color);
+        background-color: var(--dark-primary-color);
+        font-size: 2rem;
+
+        &:hover {
+          background-color: var(--accent-color);
+        }
+      }
+    }
+  }
+
+  .lesson-sidebar {
+    font-size: 2rem; 
+
+    &__container {
+      display: grid;
+      grid-template-rows: [content-start] 1fr [content-end
+                          input-start] min-content [input-end];
+
+      height: 100%;
+    }
+
+    &__content {
+      text-align: left;
+      overflow: auto;
+    }
+
+    &__input__container {
+      display: flex;
+      flex-direction: column;
+    }
+
+    &__input {
+      font-size: 1.8rem;
+      margin-bottom: 1rem;
+    }
+
+    &__submit {
+      border: 1px solid var(--primary-color);
+      background-color: var(--dark-primary-color);
+      color: var(--text);
+      font-size: 1.4rem;
+
+      &:hover {
+        background-color: var(--accent-color);
+      }
+    }
+  }
 </style>
